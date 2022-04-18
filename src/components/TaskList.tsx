80ef01ee166a92cfb61376f56f0ea42687d43a5a
eireneof <eof@ic.ufal.br>
2022-04-18 01:57:25 -0300

    :tada: challenge finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(!newTaskTitle) return;
 
     const newTask = {
@@ -30,7 +29,6 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     const newTasks = tasks.map(task => task.id === id ? {
       ...task,
       isComplete: !task.isComplete,
@@ -40,7 +38,6 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
     const otherTasks = tasks.filter(task => task.id !== id);
     setTasks(otherTasks);
   }
